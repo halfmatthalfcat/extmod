6c352d4d1f7df412acbd96e89dc188ceb6a87c6d
Matt Oliver <moliver@arrivelogistics.com>
2023-09-14 15:49:20 -0500

    WIP

--- packages/core/loader/preload.ts
+++ packages/core/loader/preload.ts
@@ -1,3 +1,9 @@
+/**
+ * globalPreload is used to expose communication between the loader and downstream
+ * applications importing things. Most notibly, this is used for Client Components and
+ * React Suspense to notify Suspense when we've fully bundled, and to then render appropriately.
+ * */
+
 /// <reference types="typings-esm-loader" />
 
 export let port: Parameters<globalPreload>[0]["port"] | undefined;
